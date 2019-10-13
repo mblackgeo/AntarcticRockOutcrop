fa8a52d3519446bae2af01122c5f15bb62da71c6
Sam <samuel.elkind@gmail.com>
2019-10-13 08:33:35 -0700

    whitespace closer to PEP8

--- antarctic_rock_outcrop_os.py
+++ antarctic_rock_outcrop_os.py
@@ -105,98 +105,98 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import sys, os, time
 
-#set arcpy environment variables
-#arcpy.env.overwriteOutput = True #uncomment to allow arcpy to overwrite outputs
+# set arcpy environment variables
+# arcpy.env.overwriteOutput = True #uncomment to allow arcpy to overwrite outputs
 
-#inputs (tile list file, directory containing tiles, coastline for masking)
-landsatTileList  = r"C:/path/to/landsatTilesToProcess.txt"
+# inputs (tile list file, directory containing tiles, coastline for masking)
+landsatTileList = r"C:/path/to/landsatTilesToProcess.txt"
 landsatDirectory = r"C:/path/to/folder/of/all/landsat/tiles"
 coastMaskShpfile = r"C:/path/to/coastline.shp" 
 
-#outputs (file extension, output directory)
+# outputs (file extension, output directory)
 outputDirectory = r"C:/path/to/outputs"
-outputFileExt   = r"_fileSuffix.tif"
+outputFileExt = r"_fileSuffix.tif"
 
-#create tile ID list from an ESPA style text file
+# create tile ID list from an ESPA style text file
 # TODO create alternative ways to source tile IDs (db query, api request, os.listdir)
 tiles = open(landsatTileList).read().splitlines()
 
-#print the start time
+# print the start time
 startTime = time.ctime()
 # TODO add logging for script
 print "ArcPy Rock masking script started: %s" % startTime
 
-#loop through each raster
+# loop through each raster
 for i in range(len(tiles)):
 
-	#set up a couple of filename strings we'll need later
-	thisTileID   = tiles[i]
+	# set up a couple of filename strings we'll need later
+	thisTileID = tiles[i]
 	thisTileFile = landsatDirectory + "/" + thisTileID
-	outFilename  = outputDirectory  + "/" + thisTileID
+	outFilename = outputDirectory + "/" + thisTileID
 
-	#print the file we're doing
+	# print the file we're doing
 	fmt = "\t%d of %d."
-	print( fmt % ( i+1, len(tiles) ) ),
-	tic=time.time();	
+	print(fmt % (i + 1, len(tiles))),
+	tic = time.time();	
 
-	#grab the band data (LANDSAT-8 OLI, bands are stacked, no Panchromatic band)
-	B2  = Raster(thisTileFile + "_toa_band2.tif")  #Blue
-	B3  = Raster(thisTileFile + "_toa_band3.tif")  #Green
-	B5  = Raster(thisTileFile + "_toa_band5.tif")  #NIR
-	B6  = Raster(thisTileFile + "_toa_band6.tif")  #SWIR1
-	B10 = Raster(thisTileFile + "_toa_band10.tif") #TIRS1
+	# grab the band data (LANDSAT-8 OLI, bands are stacked, no Panchromatic band)
+	B2 = Raster(thisTileFile + "_toa_band2.tif")  # Blue
+	B3 = Raster(thisTileFile + "_toa_band3.tif")  # Green
+	B5 = Raster(thisTileFile + "_toa_band5.tif")  # NIR
+	B6 = Raster(thisTileFile + "_toa_band6.tif")  # SWIR1
+	B10 = Raster(thisTileFile + "_toa_band10.tif") # TIRS1
 
-	#float each raster
-	B2  = Float(B2)
-	B3  = Float(B3)
-	B5  = Float(B5)
-	B6  = Float(B6)
+	# float each raster
+	B2 = Float(B2)
+	B3 = Float(B3)
+	B5 = Float(B5)
+	B6 = Float(B6)
 	B10 = Float(B10)
 
-	#extract by mask on coastline
+	# extract by mask on coastline
 	coastMask = ExtractByMask(B2, coastMaskShpfile)
 	coastMaskBin = coastMask > 0
 
-	#print the coast mask
-	toc=time.time();
-	print(" Loaded & Coast Masked (%.02fs)." % (toc-tic)),
-	tic=time.time();		
+	# print the coast mask
+	toc = time.time();
+	print(" Loaded & Coast Masked (%.02fs)." % (toc - tic)),
+	tic = time.time();		
 
-	#ratios
-	ndsi = (B3 - B6)/(B3 + B6)
-	ndwi = (B3 - B5)/(B3 + B5)
+	# ratios
+	ndsi = (B3 - B6) / (B3 + B6)
+	ndwi = (B3 - B5) / (B3 + B5)
 
-	#mask 1, sunlit rock
-	mask1_step1 = ( B10/B2 ) > 0.4
+	# mask 1, sunlit rock
+	mask1_step1 = (B10 / B2) > 0.4
 	mask1_step2 = ndsi < 0.75
 	mask1_step3 = ndwi < 0.45
-	mask1_step5 = B10 > 2550 #note this is a scaled value
+	mask1_step5 = B10 > 2550 # note this is a scaled value
 	mask1_prefinal = Int(mask1_step1) + Int(mask1_step2) + Int(mask1_step3) + Int(coastMaskBin) + Int(mask1_step5)
-	mask1_final    = mask1_prefinal == 5
+	mask1_final = mask1_prefinal == 5
 
-	#mask 2, rock in shade
-	mask2_step1 = B2 < 2500 #note this is a scaled value
+	# mask 2, rock in shade
+	mask2_step1 = B2 < 2500 # note this is a scaled value
 	mask2_step2 = ndwi < 0.45
-	mask2_prefinal = Int(mask2_step1) +  Int(mask2_step2) + Int(coastMaskBin)
-	mask2_final    = mask2_prefinal == 3
+	mask2_prefinal = Int(mask2_step1) + Int(mask2_step2) + Int(coastMaskBin)
+	mask2_final = mask2_prefinal == 3
 
-	#combine mask1 and mask2
+	# combine mask1 and mask2
 	mask_prefinal = mask1_final + mask2_final;
-	mask_final    = mask_prefinal > 0
+	mask_final = mask_prefinal > 0
 
-	#print the processing time
-	toc=time.time();
-	print(" Processed (%.02fs)." % (toc-tic)),	
-	tic=time.time();
+	# print the processing time
+	toc = time.time();
+	print(" Processed (%.02fs)." % (toc - tic)),	
+	tic = time.time();
 
-	#save output
+	# save output
 	mask_final.save(outFilename + outputFileExt)
 
-	#print the save time
-	toc=time.time();
-	print(" Saved (%.02fs)." % (toc-tic))	
+	# print the save time
+	toc = time.time();
+	print(" Saved (%.02fs)." % (toc - tic))	
 
-#script closing output 
+# script closing output 
 endTime = time.ctime()
 print "ArcPy Rock masking script finished: %s" % endTime
-###############################################################################
+
