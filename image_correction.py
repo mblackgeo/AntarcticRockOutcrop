c10e89f2c882696f8fb91066555ce70298f460eb
Sam <samuel.elkind@gmail.com>
2019-10-15 13:24:24 -0700

    file-specific prefixes added

--- image_correction.py
+++ image_correction.py
@@ -9,6 +9,13 @@ import os
 
 class LandsatTOACorrecter:
 
+    # These values are specific to MTL.txt file provided by AWS (and maybe google too?)
+    self.K1_PREFIX = "K1_CONSTANT_BAND_"
+    self.K2_PREFIX = "K2_CONSTANT_BAND_"
+    self.REFLECTANCE_MULT_PREFIX = "REFLECTANCE_MULT_BAND_"
+    self.REFLECTANCE_ADD_PREFIX = "REFLECTANCE_ADD_BAND_"
+    self.SUN_ELEV_PREFIX = "SUN_ELEVATION"
+
 
     """
     @param str scene_path: The absolute path to directory of a specific landsat scene containing
@@ -23,6 +30,12 @@ class LandsatTOACorrecter:
         self.mtl_path = ""
         self.configure_paths()
 
+        # convert dict to named tuple eventually
+        self.refl_vars = {}
+        self.K1 = {"K1_CONSTANT_BAND": None, "K1_CONSTANT_BAND_11": None,}
+                
+        self.K2 = {}
+
     def configure_paths(self):
         assert os.path.exists(self.path)
         assert os.path.isdir(self.path)
@@ -31,9 +44,14 @@ class LandsatTOACorrecter:
         self.mtl_path = self.base_dir + "/" + self.scene_id + "/" + self.scene_id + "_MTL.txt"
         assert os.path.exists(self.mtl_path)
 
+    def gather_correction_vars(self):
+
+        with 
+
 
 
 if __name__ == "__main__":
     test_img_path = "/home/dsa/DSA/images/LC82201072015017LGN00"
 
     test = LandsatTOACorrecter(test_img_path)
+
