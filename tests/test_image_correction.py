2143e73877fff6240136b9bb4823a37f2ea08818
Sam <samuel.elkind@gmail.com>
2019-10-19 16:21:42 -0700

    file paths and imports fixed

--- tests/test_image_correction.py
+++ tests/test_image_correction.py
@@ -1,16 +1,16 @@
 import unittest
-from image_correction import LandsatTOACorrecter
+from src.utils.image_correction import LandsatTOACorrecter
 
 
 class LandsatTOACorrecterTest(unittest.TestCase):
     def setUp(self) -> None:
-        self.valid_path = "/home/dsa/DSA/images/LC82201072015017LGN00"
+        self.valid_path = "/home/dsa/DSA/images_manual/LC82201072015017LGN00"
         self.test_scene_id = "LC82201072015017LGN00"
-        self.test_base_dir = "/home/dsa/DSA/images"
-        self.valid_file_prefix = "/home/dsa/DSA/images/LC82201072015017LGN00/LC82201072015017LGN00"
-        self.valid_mtl_path = "/home/dsa/DSA/images/LC82201072015017LGN00/LC82201072015017LGN00_MTL.txt"
+        self.test_base_dir = "/home/dsa/DSA/images_manual"
+        self.valid_file_prefix = "/home/dsa/DSA/images_manual/LC82201072015017LGN00/LC82201072015017LGN00"
+        self.valid_mtl_path = "/home/dsa/DSA/images_manual/LC82201072015017LGN00/LC82201072015017LGN00_MTL.txt"
 
-        self.invalid_path = "/home/dsa/DSA/images/LC82201072015017KGN00"
+        self.invalid_path = "/home/dsa/DSA/images_manual/LC82201072015017KGN00"
 
     def test_configure_valid_paths_scene_id(self):
         test = LandsatTOACorrecter(self.valid_path)
