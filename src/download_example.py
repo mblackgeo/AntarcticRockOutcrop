c6cfe7662c9689ceb4595d37fd8155cd6feeda28
Sam <samuel.elkind@gmail.com>
2019-10-20 11:48:23 -0700

    burjo model refactored into script. Runs successfully on 512 square raster, but memory issues when run on whole tile

--- src/download_example.py
+++ src/download_example.py
@@ -14,18 +14,25 @@ sys.path.append(os.path.join(os.path.dirname(code_dir), "landsat-util"))
 from landsat.downloader import Downloader
 from src.utils.data_directory_manager import DataDirectoryManager
 from src.utils.image_correction import LandsatTOACorrecter
+from src.models.antarctic_rock_outcrop_os import OutcropLabeler
 
 if __name__ == "__main__":
     # project_dir = os.getcwd()
     project_dir = "/home/dsa/DSA/test_project"
     dir_manager = DataDirectoryManager(project_dir)
+
     test_scene = [dir_manager.load_scene_ids()[0]["ID"]]
+    test_scene_raw = os.path.join(dir_manager.raw_image_dir, test_scene[0])
+    test_scene_corrected = os.path.join(dir_manager.corrected_image_dir, test_scene[0])
 
     downloader = Downloader(download_dir=dir_manager.download_dir)
+
     #downloader.download(test_scene)
     # this will take a while
     # dir_manager.untar_scenes(test_scene)
-    correcter = LandsatTOACorrecter(os.path.join(dir_manager.raw_image_dir, test_scene[0]))
-    correcter.correct_toa_brightness_temp(dir_manager.corrected_image_dir)
-    correcter.correct_toa_reflectance(dir_manager.corrected_image_dir)
-    print(dir_manager.corrected_image_dir)
+    correcter = LandsatTOACorrecter(test_scene_raw)
+    # correcter.correct_toa_brightness_temp(dir_manager.corrected_image_dir)
+    # correcter.correct_toa_reflectance(dir_manager.corrected_image_dir)
+
+    labeler = OutcropLabeler(test_scene_corrected)
+    # labeler.write_mask_file(dir_manager.label_dir)
