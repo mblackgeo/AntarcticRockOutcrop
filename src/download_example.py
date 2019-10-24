047e9689783496adf717de96189f754fca69c883
Sam <samuel.elkind@gmail.com>
2019-10-24 11:14:49 -0700

    coast shapefile download and extraction automated

--- src/download_example.py
+++ src/download_example.py
@@ -1,40 +1,48 @@
 import os
-# get the path to the repo directory. Might be better to use os.path.basename(sys.path[0])
-code_dir = os.path.dirname(os.path.abspath(__file__))
-
-import sys
 """
 add landsat-util submodule to system path so we can use the Downloader object
 make sure the landsat-util/requirements.txt is deleted or renamed 
 this is a hack to get around the dependency problems we've been having with
 the landsat-util repo
 """
-sys.path.append(os.path.join(os.path.dirname(code_dir), "landsat-util"))
 
-from landsat.downloader import Downloader
+from landsatUtil.landsat.downloader import Downloader
 from utils.data_directory_manager import DataDirectoryManager
 from utils.image_correction import LandsatTOACorrecter
 from models.antarctic_rock_outcrop_os import OutcropLabeler
 
 if __name__ == "__main__":
-    # project_dir = os.getcwd()
-    project_dir = "/home/dsa/DSA/test_project"
-    coast_shape_file = "/home/dsa/DSA/vectors/Coastline_high_res_polygon_v7.1.shp"
-    assert os.path.exists(coast_shape_file)
-    dir_manager = DataDirectoryManager(project_dir)
+    project_dir = os.path.join(os.getcwd(), "data")
+    # project_dir = "/home/dsa/DSA/test_project"
+    dm = DataDirectoryManager(project_dir)
 
-    test_scene = [dir_manager.load_scene_ids()[0]["ID"]]
-    test_scene_raw = os.path.join(dir_manager.raw_image_dir, test_scene[0])
-    test_scene_corrected = os.path.join(dir_manager.corrected_image_dir, test_scene[0])
+    # download and extract coast shapefile from antarctic digital database
+    dm.download_coast_shapefile()
+    dm.extract_coast_shapefile()
 
-    # downloader = Downloader(download_dir=dir_manager.download_dir)
+    # download and extract tab delimited text file containing Scene IDs used in Burton-Johnson study
+    dm.download_supplement()
+    dm.extract_scene_id_file()
 
-    #downloader.download(test_scene)
+    # Load a scene id from file and save it as a list with len() = 1
+    scene_IDs = [dm.load_scene_ids()[0]["ID"]]
+
+    # Create directory name to hold raw scene bands
+    test_scene_raw = os.path.join(dm.raw_image_dir, scene_IDs[0])
+
+    # Create directory name to hold corrected scene bands
+    test_scene_corrected = os.path.join(dm.corrected_image_dir, scene_IDs[0])
+
+    # Create landsat util scene downloader
+    downloader = Downloader(download_dir=dm.download_dir)
+
+    downloader.download(scene_IDs)
     # this will take a while
-    # dir_manager.untar_scenes(test_scene)
-    # correcter = LandsatTOACorrecter(test_scene_raw)
-    # correcter.correct_toa_brightness_temp(dir_manager.corrected_image_dir)
-    # correcter.correct_toa_reflectance(dir_manager.corrected_image_dir)
+    dm.untar_scenes(scene_IDs)
 
-    labeler = OutcropLabeler(test_scene_corrected, coast_shape_file)
-    labeler.write_mask_file(dir_manager.label_dir)
+    correcter = LandsatTOACorrecter(test_scene_raw)
+    correcter.correct_toa_brightness_temp(dm.corrected_image_dir)
+    correcter.correct_toa_reflectance(dm.corrected_image_dir)
+
+    labeler = OutcropLabeler(test_scene_corrected, dm.coast_shape_path)
+    labeler.write_mask_file(dm.label_dir)
--- src/utils/data_directory_manager.py
+++ src/utils/data_directory_manager.py
@@ -16,6 +16,12 @@ class DataDirectoryManager:
 
     SCENE_ID_FILE = "burton_johnson_scene_ids.txt"
 
+    COAST_URL = "https://add.data.bas.ac.uk/repository/entry/get/Coastline_high_res_polygon_v7.1.zip?entryid=synth%3Af477219b-9121-44d6-afa6-d8552762dc45%3AL2NvYXN0bGluZXMvc2hwL0NvYXN0bGluZV9oaWdoX3Jlc19wb2x5Z29uX3Y3LjEuemlw"
+    COAST_ZIP_NAME = "coastline.zip"
+
+    COAST_DIR_NAME = "coastline"
+    COAST_SHAPEFILE = "Coastline_high_res_polygon_v7.1.shp"
+
     RAW_IMAGES = "raw"
     CORRECTED_IMAGES = "corrected"
     OUTCROP_LABELS = "labels"
@@ -32,10 +38,14 @@ class DataDirectoryManager:
         self.corrected_image_dir = os.path.join(self.project_dir, self.CORRECTED_IMAGES)
         self.label_dir = os.path.join(self.project_dir, self.OUTCROP_LABELS)
         self.download_dir = os.path.join(self.project_dir, self.DOWNLOADS)
+        self.coast_dir = os.path.join(self.project_dir, self.COAST_DIR_NAME)
         self.configure_data_dirs()
 
         self.zip_path = os.path.join(self.project_dir, self.ZIP_NAME)
         self.scene_id_file = os.path.join(self.project_dir, self.SCENE_ID_FILE)
+
+        self.coast_zip_path = os.path.join(self.project_dir, self.COAST_ZIP_NAME)
+        self.coast_shape_path = os.path.join(self.coast_dir, self.COAST_SHAPEFILE)
 
     def configure_logger(self):
         logger = logging.getLogger('scene_downloader_log')
@@ -53,7 +63,7 @@ class DataDirectoryManager:
         return logger
 
     def configure_data_dirs(self):
-        for i in [self.raw_image_dir, self.corrected_image_dir, self.label_dir, self.download_dir]:
+        for i in [self.raw_image_dir, self.corrected_image_dir, self.label_dir, self.download_dir, self.coast_dir]:
             if not os.path.exists(i):
                 os.mkdir(i)
                 self.logger.info("Data directory created at {}".format(i))
@@ -82,6 +92,16 @@ class DataDirectoryManager:
                 file.write(response.read())
 
         self.logger.info("supplementary material successfully downloaded to {}".format(self.zip_path))
+
+    def download_coast_shapefile(self):
+        assert self.project_dir is not None
+
+        if not os.path.exists(self.project_dir):
+            os.makedirs(self.project_dir)
+
+        with urllib.request.urlopen(self.COAST_URL) as response:
+            with open(self.coast_zip_path, "wb") as file:
+                file.write(response.read())
 
     """
     This function unzips the supplementary material zip file, moves and renames the 
@@ -117,6 +137,14 @@ class DataDirectoryManager:
 
         os.rmdir(path_to_remove)
         self.logger.info("directory {} removed".format(path_to_remove))
+
+    def extract_coast_shapefile(self):
+        assert os.path.exists(self.coast_zip_path)
+
+        with zipfile.ZipFile(self.coast_zip_path) as zf:
+            zf.extractall(self.coast_dir)
+
+        self.logger.info("coast shapefile extracted to {}".format(self.coast_dir))
 
     def load_scene_ids(self):
         assert os.path.exists(self.scene_id_file)
