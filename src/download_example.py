4820a2486fa7aab118a4f24672abe2f8477a54b9
Sam <samuel.elkind@gmail.com>
2019-10-24 14:09:30 -0700

    logging added to downloader example

--- src/download_example.py
+++ src/download_example.py
@@ -16,16 +16,9 @@ if __name__ == "__main__":
     # project_dir = "/home/dsa/DSA/test_project"
     dm = DataDirectoryManager(project_dir)
 
-    # download and extract coast shapefile from antarctic digital database
-    dm.download_coast_shapefile()
-    dm.extract_coast_shapefile()
-
-    # download and extract tab delimited text file containing Scene IDs used in Burton-Johnson study
-    dm.download_supplement()
-    dm.extract_scene_id_file()
-
     # Load a scene id from file and save it as a list with len() = 1
     scene_IDs = [dm.load_scene_ids()[0]["ID"]]
+    dm.logger.info("Example scene ID: {}".format(scene_IDs))
 
     # Create directory name to hold raw scene bands
     test_scene_raw = os.path.join(dm.raw_image_dir, scene_IDs[0])
